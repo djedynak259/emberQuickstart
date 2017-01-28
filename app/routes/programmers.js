5b111ac762bffcef7f1316f54b2c7e334204d9dd
djedynak259 <dajed@outlook.com>
2017-01-28 10:46:31 -0800

    cli add

--- app/routes/programmers.js
+++ app/routes/programmers.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-    	return ['Mariess Curie', 'Maess Jemison', 'Albertss Hofmann'];
-  }
+    	return ['Mariess Curie', 'Maess Jemison', 'Albertsss Hofmann'];
+	}
 });
